b6776786f4be8f2a04f73c8b706003cfb08cc3b7
sivasuriyankumarasamy <sivasuriyankumarasamy@gmail.com>
2022-08-12 23:30:36 +0530

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
-
+import Header from "./Header/Header.js";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
