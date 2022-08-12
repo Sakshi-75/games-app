8bc1d89510163dd7a5254135d9343b1283b9407c
Sakshi <sakshi.jain7597@gmail.com>
2022-08-12 23:49:41 +0530

    created new game component

---
+++ src/GuessTheNumber/GuessTheNumber.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './GuessTheNumber.module.css';
+
+const GuessTheNumber = () => (
+  <div className={styles.GuessTheNumber}>
+    GuessTheNumber Component
+  </div>
+);
+
+GuessTheNumber.propTypes = {};
+
+GuessTheNumber.defaultProps = {};
+
+export default GuessTheNumber;
