6ce3be77bd033c4b936b29967d32015bd3c6804c
Sakshi <sakshi.jain7597@gmail.com>
2022-08-16 23:15:02 +0530

    updated app name in header

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./Header.module.css";
 
-const Header = () => <div className={styles.Header}>Header Component</div>;
+const Header = () => <div className={styles.Header}>Multiple Games</div>;
 
 Header.propTypes = {};
 
