18ee469df86ec317cdd0efbe9a4d946863402d8d
Sakshi <sakshi.jain7597@gmail.com>
2022-08-17 22:04:40 +0530

    added back button

--- src/ThreeDiceThrow/ThreeDiceThrow.js
+++ src/ThreeDiceThrow/ThreeDiceThrow.js
@@ -1,8 +1,15 @@
 import React from "react";
 import styles from "./ThreeDiceThrow.module.css";
+import { Link } from "react-router-dom";
 
-const ThreeDiceThrow = () => (
-  <div className={styles.ThreeDiceThrow}>ThreeDiceThrow Component</div>
-);
+const ThreeDiceThrow = () => {
+  return (
+    <div className={styles.ThreeDiceThrow}>
+      <Link to={"/"}>
+        <button>Back</button>
+      </Link>
+    </div>
+  );
+};
 
 export default ThreeDiceThrow;
