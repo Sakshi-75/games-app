de4bb129304c3cec7b66bfb73462fac8d852ae45
Sakshi <sakshi.jain7597@gmail.com>
2022-09-26 01:25:03 +0530

    added the submit button

--- src/HigherRoll/HigherRoll.js
+++ src/HigherRoll/HigherRoll.js
@@ -6,11 +6,20 @@ import Select from "react-select";
 const HigherRoll = () => {
   const [userDice, setUserDice] = useState();
   const [compDice, setCompDice] = useState();
+  const [hasSubmitted, updateHasSubmitted] = useState(false);
+
   const options = [
     { value: "dice1", label: "Dice 1 (3, 4, 8)" },
     { value: "dice2", label: "Dice 2 (1, 5, 9)" },
     { value: "dice3", label: "Dice 3 (2, 6, 7)" },
   ];
+
+  const checkForDisabled = () => {
+    if (!hasSubmitted && userDice !== undefined) {
+      return false;
+    }
+    return true;
+  };
   return (
     <div className={styles.HigherRoll}>
       <Link to={"/"}>
@@ -21,12 +30,22 @@ const HigherRoll = () => {
           id="userInput"
           options={options}
           getOptionValue={(option) => `$(option.value)`}
+          disabled={hasSubmitted}
           onChange={(e) => {
             const val = e.value;
             console.log(val);
             setUserDice(val);
           }}
         />
+        <button
+          type="submit"
+          disabled={checkForDisabled()}
+          onClick={() => {
+            updateHasSubmitted(true);
+          }}
+        >
+          Submit
+        </button>
       </div>
     </div>
   );
